ce506097b9e6db9493e3e0392dd39388aa926a90
Jeferson Veiga <jeferson.veiiga@icloud.com>
2022-01-08 21:27:29 -0300

    implementing comunication with api json-server

--- web/src/pages/Promotion/Search/Search.js
+++ web/src/pages/Promotion/Search/Search.js
@@ -1,20 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PromotionCard from 'components/Promotion/Card/Card';
+import axios from 'axios'
 
 import '../../../components/Promotion/Card/Card.css'
 
-const promotion = { 
-    "id":1, 
-    "title":"conta numero um",
-    "price":9.99,
-
-}
 
 const PagesPromotionSearch = () => {
+  const [promotions, setPromotions] = useState([])
+
+  useEffect(() => { 
+    axios.get('http://localhost:5000/promotions')
+      .then((response) => {
+        setPromotions(response.data);
+      });
+  }, []);
+
+
   return (
     <>
       <h1>Despesas</h1>
-      <PromotionCard promotion={promotion} />
+      {promotions.map((promotion) => (
+        <PromotionCard promotion={promotion} />
+      ))}
     </>
   );
 }
